8284e75c0db194629db1d778f4b0b813c6b183ea
Shahidul-Islam-Leon <72728147+Shahidul-Islam-Leon@users.noreply.github.com>
2021-02-15 14:09:12 +0600

    1st commit

---
+++ lab 4/submited.php
@@ -0,0 +1,21 @@
+<html>
+
+<body>
+    <h1>
+        From submited
+    </h1>
+    <?php
+			echo "Name: ". $_POST["uname"]."<br>";
+			echo "Password: ". $_POST["pass"]."<br>";
+			echo "Gender: ".$_POST["gender"]."<br>";
+			echo "Profession: ".$_POST["profession"]."<br>";
+			echo "Bio: ".$_POST["bio"]."<br>";
+			$var = $_POST["hobbies"];
+			for($i=0;$i<count($var);$i++){
+				echo $var[$i]."<br>";
+			}
+			
+		?>
+</body>
+
+</html>
